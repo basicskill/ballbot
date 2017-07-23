e40205898c7996820e4f1ae11228740a8e651667
basicskill <mladenbasic99@gmail.com>
2017-07-24 00:07:10 +0200

    Rootlocus za inverzno klatno

--- inverzno_klatno.m
+++ inverzno_klatno.m
@@ -1,8 +1,9 @@
+close all
 M = 0.5;
 m = 0.2;
 b = 0.1;
 I = 0.006;
-g = 9.8;
+g = 9.81;
 l = 0.3;
 s = tf('s');
 Kp = 10;
@@ -16,11 +17,35 @@ q = (M+m)*(I+m*l^2)-(m*l)^2;
 Tf_klatno = (m*l*s/q)/(s^3 + (b*(I + m*l^2))*s^2/q - ...
     ((M + m)*m*g*l)*s/q - b*m*g*l/q);
 
-kontroler = pid(Kp, Ki, Kd);
+kontroler = (1/s) * (s + 4)*(s + 3);
+HG = kontroler * Tf_klatno;
+
+HG = minreal(HG);
+
+polovi = pole(HG);
+
+rlocus(HG);
+sgrid(0.9, 5)
+
+K = 12.5; 
+kontroler = K * kontroler;
 
 TF = feedback(Tf_klatno, kontroler);
+figure(2)
+impulse(TF);
 
-impulse(TF, t)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- napon_motor.m
+++ napon_motor.m
@@ -1,13 +1,11 @@
 function y = napon_motor(t)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-step = 5;
+    step = 5;
 
-if (t>0)
-    y = step;
-else
-    y = 0;
-end
+    if (t>0)
+        y = step;
+    else
+        y = 0;
+    end
 
 end
 
